fb12082ca06cf84720f576b6d103e24318bf8295
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-20 13:26:49 -0600

    feat(Theme): add assets and color

--- app/src/main/java/com/example/weathertoday/ui/theme/Color.kt
+++ app/src/main/java/com/example/weathertoday/ui/theme/Color.kt
@@ -2,7 +2,19 @@ package com.example.weathertoday.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue600 = Color(0xFF2F90FF)
+val Blue500= Color(0xFF00BCFF)
+val Blue400 = Color(0xFF5AB9F3)
+val Blue300 = Color(0xFF7EB9C5)
+val Blue200 = Color(0xFFB2D7DE)
+val Blue100 = Color(0xFFD0F3FF)
+val Gray900 = Color(0xFF29292C)
+val Gray800 = Color(0xFF333333)
+val Gray400 = Color(0xFFA0A7BA)
+val Gray300 = Color(0xFFCDD2DE)
+val Gray200 = Color(0xFFCBD6E7)
+val White = Color(0xFFFFFFFF)
+val Yellow500 = Color(0xFFFFD439)
+val Green500 = Color(0xFF37E4AA)
+val Green400 = Color(0xFF68E2D3)
+val Red500 = Color(0xFFFD7778)
