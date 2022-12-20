65141459c783d40bc303bacfbce654512d09b6ba
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-20 13:27:16 -0600

    feat(Theme): add typography

--- app/src/main/java/com/example/weathertoday/ui/theme/Theme.kt
+++ app/src/main/java/com/example/weathertoday/ui/theme/Theme.kt
@@ -7,18 +7,18 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue500,
+    primaryVariant = Blue600,
+    secondary = Green400
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue500,
+    primaryVariant = Blue600,
+    secondary = Green400,
+    background = White,
 
     /* Other default colors to override
-    background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
--- app/src/main/java/com/example/weathertoday/ui/theme/Type.kt
+++ app/src/main/java/com/example/weathertoday/ui/theme/Type.kt
@@ -2,27 +2,49 @@ package com.example.weathertoday.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.weathertoday.R
 
 // Set of Material typography styles to start with
+
+val fonts = FontFamily(
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+    Font(R.font.inter_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+)
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 28.8.sp,
+        fontSize = 24.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    subtitle1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 24.sp,
+        fontSize = 20.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        fontSize = 20.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 19.2.sp,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 19.2.sp,
+        fontSize = 16.sp
+    ),
 )
