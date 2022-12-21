fe22226dcc9d486fd0ca27b431d9f7bbcbd509f8
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-21 15:21:00 -0600

    feat(ShimmerEffect): add component

---
+++ app/src/main/java/com/weathertoday/shared/presentation/components/shimmer/ShimmerEffect.kt
@@ -0,0 +1,38 @@
+package com.weathertoday.shared.presentation.components.shimmer
+import androidx.compose.animation.core.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import com.weathertoday.ui.theme.AppTheme
+
+@Composable
+fun AnimatedShimmer(child: @Composable (brush: Brush) -> Unit) {
+    val shimmerColors = listOf(
+        AppTheme.colors.Gray200,
+        AppTheme.colors.Gray100,
+        AppTheme.colors.Gray200,
+    )
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    child(brush)
+}
+
+
---
+++ app/src/main/java/com/weathertoday/shared/presentation/components/shimmer/skeletons/WheaterDetailSkeleton.kt
@@ -0,0 +1,36 @@
+package com.weathertoday.shared.presentation.components.shimmer.skeletons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
+import com.weathertoday.shared.presentation.components.shimmer.AnimatedShimmer
+
+@Composable
+fun WeatherDetailSkeleton() {
+    AnimatedShimmer(child = {Skeleton(it)})
+}
+
+@Composable
+private fun Skeleton(brush: Brush) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(32.dp))
+                .background(brush)
+                .height(316.dp)
+
+        )
+    }
+}
